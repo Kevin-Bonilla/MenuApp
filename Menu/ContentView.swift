a46c50f8a26b96a90d50e8c46e82bd3e7b0edb5b
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-02-16 22:47:24 -0600

    Initial Commit

---
+++ Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Menu
+//
+//  Created by Kevin Bonilla on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Kevin Bonilla on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
