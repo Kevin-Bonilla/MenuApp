bc265cee13b08220c7f12e842e52df722316c15e
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-02-18 16:12:46 -0600

    MenuItem Struct added

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Kevin Bonilla on 2/16/26.
+//
+
+import Foundation
+
+struct MenuItem{
+    var name: String
+    var price: String
+    var imageName: String
+}
